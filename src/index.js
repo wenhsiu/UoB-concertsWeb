4408caa7e9e8c33eb600a2fe4e15522f26df240f
Wen-Hsiu Hsu <alvina8418@gmail.com>
2019-06-23 16:44:00 +0100

    Add web logo.svg and modified header.css

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Header from './header';
+import Header from './header/Header';
+import Datepicker from './date_picker/Datepicker';
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(<Header />, document.getElementById('header'));
+ReactDOM.render(<Datepicker />, document.getElementById('date_picker'));
 // ReactDOM.render(<Header />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
